abe61fa981dd54c079f8fed11e045111dac98026
faisalnur7 <md.faisalnur55@gmail.com>
2017-08-23 17:21:29 +0600

    doc added to index method

--- root/app/Http/Controllers/BrandController.php
+++ root/app/Http/Controllers/BrandController.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Session;
 
 class BrandController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $brands = Brand::all();
